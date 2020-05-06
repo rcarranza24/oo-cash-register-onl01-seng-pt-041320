506e28a0e9ca808e321b6ea1197c5cde35229259
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-06 23:55:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,16 +10,16 @@ class CashRegister
    end
 
     def add_item(title, amount, quantity=1)
-     self.total += amount * quantity
+     @total += amount * quantity
      quantity.times do
        items << title
      end
-     self.last_transaction = amount * quantity
+     @last_transaction = amount * quantity
    end
 
     def apply_discount
      if discount != 0
-       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+       @total = (total * ((100.0 - discount.to_f)/100)).to_i
        "After the discount, the total comes to $#{self.total}."
      else
        "There is no discount to apply."
@@ -27,6 +27,6 @@ class CashRegister
    end
 
     def void_last_transaction
-     self.total = self.total - self.last_transaction
+     @total = @total - @last_transaction
    end
  end
